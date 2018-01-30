168445de988f40a0d67b005303b082408fb95d35
d-kang <david.kang714@gmail.com>
2018-01-30 07:21:37 -0800

    Refactor Cells Component

--- src/components/Cells.jsx
+++ src/components/Cells.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Cells = ({ subArr }) => {
-  return (
-    subArr.map((block, k) => {
-      const color = block === 0 ? 'white'
-        : block === 1 ? 'grey'
-        : block === 2 ? 'cyan'
-        : 'black';
-      return (
-        <div className={`cell ${color}`} key={k}></div>
-      )
-    })
-  )
-}
+const colorFinder = num => (
+  num === 0 ? 'white'
+    : num === 1 ? 'grey'
+    : num === 2 ? 'cyan'
+    : 'black'
+)
+
+
+const Cells = ({ subArr }) => (
+  subArr.map((block, k) => (
+    <div className={`cell ${colorFinder(block)}`} key={k}></div>
+  ))
+)
 
 export default Cells;
--- src/components/Rows.jsx
+++ src/components/Rows.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import Cells from './Cells';
 
-const Rows = ({ blocks }) => {
-  return (
-    blocks.map((subArr, i) => {
-      return (
-        <div className='row' key={i}>{
-          <Cells subArr={subArr} />
-        }</div>
-      )
-    })
+const Rows = ({ blocks }) => (
+  blocks.map((subArr, i) =>
+    <div className='row' key={i}>
+      <Cells subArr={subArr} />
+    </div>
   )
-}
+);
+
 
 export default Rows;
