e2a4aa116f5f8116b9acb357200ac50dd8ac725c
d-kang <david.kang714@gmail.com>
2018-01-29 20:06:21 -0800

    Scaffolding Webpack, Hot Module Replacement and Rendering React Components Complete

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+
+  render() {
+    return (
+      <div>hello!!!!!!</div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
